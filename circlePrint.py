e575e6827b125a941e431997af1b74d61290cfaf
JackyWang <339847016@qq.com>
2017-05-12 18:41:19 +0800

    create circleCount

---
+++ circlePrint.py
@@ -0,0 +1,65 @@
+def genArray(m, n):
+    a = []
+    count = 1
+    for i in range(m):
+        b = []
+        for j in range(n):
+            b.append(count)
+            count += 1
+        a.append(b)
+    return a
+
+def circleCount(a):
+    rows = len(a)
+    cols = len(a[0])
+    # print rows
+    # print cols
+    x = 0
+    y = 0
+    left = 0
+    right = cols
+    top = 0
+    down = rows
+
+    while True:
+        #print (left, right, top, down)
+
+        for i in range(right - left):
+            print a[x][y]
+            y += 1
+        top += 1
+        if top == down:
+            break
+        y -= 1
+        x += 1
+
+
+        for i in range(down - top):
+            print a[x][y]
+            x += 1
+        right -= 1
+        if right == left:
+            break
+        x -= 1
+        y -= 1
+
+        for i in range(right - left):
+            print a[x][y]
+            y -= 1
+        down -= 1
+        if top == down:
+            break
+        y += 1
+        x -= 1
+
+        for i in range(down - top):
+            print a[x][y]
+            x -= 1
+        left += 1
+        if right == left:
+            break
+        x += 1
+        y += 1
+
+a = genArray(7, 5)
+circleCount(a)
